5b251647a5e257de61cd7e1b561d812abcd5fd0f
Anthony <semprini.anthony@gmail.com>
2024-02-15 16:27:33 +0100

    mise en place de tailwind css

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -4,14 +4,16 @@
 
 @section('content')
 
-<h1>Mon blog</h1>
+<h1 class="text-center text-bold text-3xl" >Mon blog</h1>
 
 @foreach ($articles as $article)
-<p>Titre :{{ $article->Titre }}</p>
-<p>Catégorie{{ $article->Categorie }}</p>
-<p>{{ $article->Contenu }}</p>
-<p>Date de réation :{{ $article->created_at->format('d/m/Y H:i') }}</p>
-<p>Dérniere modification :{{ $article->updated_at->format('d/m/Y H:i') }}</p>
+<article>
+    <h2>Titre :{{ $article->Titre }}</h2>
+    <p>Catégorie : {{ $article->Categorie }}</p>
+    <p>{{ $article->Contenu }}</p>
+    <p>Create : {{ $article->created_at->format('d/m/Y H:i') }}</p>
+    <p>Update : {{ $article->updated_at->format('d/m/Y H:i') }}</p>
+</article>
 @endforeach
 
 @endsection
