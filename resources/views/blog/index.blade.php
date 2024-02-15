9ae3f2b6a9183e8659a50edcea4782aed7d19c9d
Anthony <semprini.anthony@gmail.com>
2024-02-15 15:26:22 +0100

    Liste des article dans index

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -7,7 +7,11 @@
 <h1>Mon blog</h1>
 
 @foreach ($articles as $article)
-<p>Le titre de l'article :{{ $article->Titre }}</p>
+<p>Titre :{{ $article->Titre }}</p>
+<p>Catégorie{{ $article->Categorie }}</p>
+<p>{{ $article->Contenu }}</p>
+<p>Date de réation :{{ $article->created_at->format('d/m/Y H:i') }}</p>
+<p>Dérniere modification :{{ $article->updated_at->format('d/m/Y H:i') }}</p>
 @endforeach
 
 @endsection
