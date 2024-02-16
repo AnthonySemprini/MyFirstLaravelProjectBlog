88f588290951b2407bcb05d42c1b673b990b08b4
Anthony <semprini.anthony@gmail.com>
2024-02-16 10:57:27 +0100

    mise en place nav bar tailw

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,10 +9,50 @@
     @vite('resources/css/app.css')
     <title>@yield('title')</title>
 </head>
+
 <body>
-    
+    <nav class="bg-gray-500">
+        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div class="relative flex h-16 items-center justify-between">
+                <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                </div>
+                <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                    <div class="flex flex-shrink-0 items-center">
+                        <img class="h-10 w-auto" src="/img/logo.png" alt="Your Company">
+                    </div>
+                    <div class="hidden sm:ml-6 sm:block">
+                        <div class="flex space-x-4">
+                            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                            <a href="{{ route('blog.index') }}"
+                                class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                                aria-current="page">Mon blog</a>
+                            <a href="#"
+                                class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Gerer
+                                les articles</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+        <!-- Mobile menu, show/hide based on menu state. -->
+        <div class="sm:hidden" id="mobile-menu">
+            <div class="space-y-1 px-2 pb-3 pt-2">
+                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                <a href="{{ route('blog.index') }}"
+                    class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+                    aria-current="page">Mon blog</a>
+                <a href="#"
+                    class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Gerer
+                    les articles</a>
+            </div>
+        </div>
+    </nav>
+
     <div class="container">
         @yield('content')
     </div>
 </body>
+
 </html>
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -4,24 +4,24 @@
 
 @section('content')
 
-<h1 class="text-center text-bold text-3xl" >Mon blog</h1>
+    <h1 class="text-center text-bold text-3xl">Mon blog</h1>
 
-@foreach ($articles as $article)
-<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl my-5">
-    <article class="p-8">
-        <h2 class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
-            <a href="{{ route('blog.show', $article->id) }}">Titre : {{ $article->Titre }}</a>
-        </h2>
-        <p class="mt-2 text-gray-500">Catégorie : {{ $article->Categorie }}</p>
-        {{-- <p class="mt-2 text-gray-600">{{ $article->Contenu }}</p> --}}
-        <div class="mt-4">
-            <p class="text-gray-500">Créé le : {{ $article->created_at->format('d/m/Y H:i') }}</p>
-            {{-- <p class="text-gray-500">Mis à jour le : {{ $article->updated_at->format('d/m/Y H:i') }}</p> --}}
+    @foreach ($articles as $article)
+        <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl my-5">
+            <article class="p-8">
+                <h2 class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+                    <a href="{{ route('blog.show', $article->id) }}">Titre : {{ $article->Titre }}</a>
+                </h2>
+                <p class="mt-2 text-gray-500">Catégorie : {{ $article->Categorie }}</p>
+                {{-- <p class="mt-2 text-gray-600">{{ $article->Contenu }}</p> --}}
+                <div class="mt-4">
+                    <p class="text-gray-500">Créé le : {{ $article->created_at->format('d/m/Y H:i') }}</p>
+                    {{-- <p class="text-gray-500">Mis à jour le : {{ $article->updated_at->format('d/m/Y H:i') }}</p> --}}
+                </div>
+            </article>
         </div>
-    </article>
-</div>
-@endforeach
+    @endforeach
 
-   
+
 
 @endsection
